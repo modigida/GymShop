fd7e46c68d2f5d2d3a16868e9ddbc1dbb5830697
Ida Modigh <modigida@hotmail.com>
2025-02-24 19:24:36 +0100

    Create PasswordHasher

---
+++ GymShopApi/Hasher/PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GymShopApi.Hasher;
+public class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public static bool VerifyPassword(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+
+    public static string ExtractSalt(string hashedPassword)
+    {
+        var parts = hashedPassword.Split('$');
+        return "$" + string.Join("$", parts.Take(3));
+    }
+}
