6444a632a8c3c21cd5ef202b9bcc4e60933e39a8
Ida Modigh <modigida@hotmail.com>
2025-02-24 19:25:06 +0100

    Create interfaces for UnitOfWork and GenericRepository

---
+++ GymShopApi/Interfaces/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace GymShopApi.Interfaces;
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(object id);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ GymShopApi/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using GymShopApi.Models;
+
+namespace GymShopApi.Interfaces;
+public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<Category> Categories { get; }
+    IGenericRepository<Order> Orders { get; }
+    IGenericRepository<OrderProduct> OrderProducts { get; }
+    IGenericRepository<Product> Products { get; }
+    IGenericRepository<Role> Roles { get; }
+    IGenericRepository<Status> Statuses { get; }
+    IGenericRepository<User> Users { get; }
+    Task<int> CompleteAsync();
+}
