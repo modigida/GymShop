fd03a8333ac92af2b830fee6db75b35a3d66a59e
Ida Modigh <modigida@hotmail.com>
2025-02-24 19:26:24 +0100

    Implement UnitOfWork and GenericRepository

---
+++ GymShopApi/Repositories/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using GymShopApi.Database;
+using GymShopApi.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymShopApi.Repositories;
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+    public async Task<T> GetByIdAsync(object id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
---
+++ GymShopApi/Repositories/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using GymShopApi.Database;
+using GymShopApi.Interfaces;
+using GymShopApi.Models;
+
+namespace GymShopApi.Repositories;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public IGenericRepository<Category> Categories { get; }
+    public IGenericRepository<Order> Orders { get; }
+    public IGenericRepository<OrderProduct> OrderProducts { get; }
+    public IGenericRepository<Product> Products { get; }
+    public IGenericRepository<Role> Roles { get; }
+    public IGenericRepository<Status> Statuses { get; }
+    public IGenericRepository<User> Users { get; }
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        Categories = new GenericRepository<Category>(_context);
+        Orders = new GenericRepository<Order>(_context);
+        OrderProducts = new GenericRepository<OrderProduct>(_context);
+        Products = new GenericRepository<Product>(_context);
+        Roles = new GenericRepository<Role>(_context);
+        Statuses = new GenericRepository<Status>(_context);
+        Users = new GenericRepository<User>(_context);
+    }
+    public async Task<int> CompleteAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
+
