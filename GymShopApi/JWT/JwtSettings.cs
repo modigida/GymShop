8f219af9a70bf34a0301735f70d76b452d96877c
Ida Modigh <modigida@hotmail.com>
2025-02-24 15:30:14 +0100

    Add JwtSettings

---
+++ GymShopApi/JWT/JwtSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GymShopApi.JWT;
+public class JwtSettings
+{
+    public required string SecretKey { get; set; }
+    public required string Issuer { get; set; }
+    public required string Audience { get; set; }
+    public required int ExpirationInMinutes { get; set; }
+}
