18d353267e1645f3c1b834695a69d15b77ab5a43
Ida Modigh <modigida@hotmail.com>
2025-02-24 19:28:57 +0100

    Register AppDbContext in Program.cs

--- GymShopApi/Program.cs
+++ GymShopApi/Program.cs
@@ -1,5 +1,7 @@
+using GymShopApi.Database;
 using GymShopApi.Interfaces;
 using GymShopApi.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Register Interfaces
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
