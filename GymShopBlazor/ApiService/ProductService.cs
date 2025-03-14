90d595ccfbc3b8948e783b24bf0f12f0154aeafc
Ida Modigh <modigida@hotmail.com>
2025-03-14 10:31:59 +0100

    Add function to display Products from database on Product.razor pages, both all products and based on category

--- GymShopBlazor/ApiService/ProductService.cs
+++ GymShopBlazor/ApiService/ProductService.cs
@@ -2,6 +2,7 @@
 using GymShopBlazor.Models;
 
 namespace GymShopBlazor.ApiService;
+
 public class ProductService(HttpClient httpClient)
 {
     public async Task<List<Product?>> GetAll()
@@ -9,9 +10,22 @@ public class ProductService(HttpClient httpClient)
         var products = await httpClient.GetFromJsonAsync<List<Product>>("https://localhost:7097/api/Products");
         return products ?? new List<Product>();
     }
+
     public async Task<Product?> GetUserById(int id)
     {
-        var product = await httpClient.GetFromJsonAsync<Product>($"api/Products/{id}");
+        var product = await httpClient.GetFromJsonAsync<Product>($"https://localhost:7097/api/Products/{id}");
         return product ?? null;
+    }
+
+    public async Task<List<Product?>> GetByCategory(Category category)
+    {
+        var products = await httpClient.GetFromJsonAsync<List<Product>>($"https://localhost:7097/api/Products/category/{category.Id}");
+        return products ?? new List<Product>();
+    }
+
+    public async Task<List<Category>> GetCategories()
+    {
+        var categories = await httpClient.GetFromJsonAsync<List<Category>>("https://localhost:7097/api/Categories");
+        return categories ?? new List<Category>();
     }
 }
