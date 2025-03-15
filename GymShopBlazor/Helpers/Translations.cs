065fc480ed78a9a7815b2300c8bc99e4f4189e15
Ida Modigh <modigida@hotmail.com>
2025-03-15 14:52:52 +0100

    Improve code reusability using static helpers translation categories and productstatuses to swedish.

---
+++ GymShopBlazor/Helpers/Translations.cs
@@ -0,0 +1,36 @@
+﻿namespace GymShopBlazor.Helpers;
+public static class Translations
+{
+    public static string GetTranslatedCategoryName(string categoryName)
+    {
+        Dictionary<string, string> categoryTranslations = new()
+        {
+            { "Clothes", "Träningskläder" },
+            { "Supplements", "Kosttillskott" },
+            { "Equipments", "Träningstillbehör" },
+        };
+
+        if (categoryTranslations.ContainsKey(categoryName))
+        {
+            return categoryTranslations[categoryName];
+        }
+        return categoryName;
+    }
+    public static string GetTranslatedProductStatus(string produtstatus)
+    {
+        Dictionary<string, string> statusTranslations = new()
+        {
+            { "Available", "Tillgänglig" },
+            { "Out of Stock", "Slut i lager" },
+            { "Discontinued", "Utgången" },
+            { "Pre-Order", "Förbeställning" },
+            { "Limited Edition", "Begränsad upplaga" }
+        };
+
+        if (statusTranslations.ContainsKey(produtstatus))
+        {
+            return statusTranslations[produtstatus];
+        }
+        return produtstatus;
+    }
+}
